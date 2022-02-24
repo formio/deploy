5406d8dbc7f99d11e346c6f2151a719455a48810
Travis Tidwell <travis@form.io>
2022-02-23 21:39:29 -0600

    Updated build.

--- commands/push.js
+++ commands/push.js
@@ -4,6 +4,27 @@ module.exports = (program) => {
         .command('push <source> <destination>').alias('p')
         .description('Push an application to a hosted environment.')
         .action(async (source, destination) => {
-            await shell(`aws s3 cp --recursive --acl public-read --exclude "*node_modules*|*\.git*" ${source}/ s3://${destination}`);
+            let cloudfront = '';
+            switch (destination) {
+                case 'portal.test-form.io':
+                    cloudfront = 'E2BZNWGLSP355U';
+                    break;
+                case 'portal.form.io':
+                    cloudfront = 'E3BVZ9SUM1E422';
+                    break;
+                case 'pro.formview.io':
+                    cloudfront = 'E1G0SRPKZNR4IC';
+                    break;
+                case 'test.formview.io':
+                    cloudfront = 'E1GH8TBBTOUUKY';
+                    break;
+                case 'apps.form.io/manager':
+                    cloudfront = 'E26X18INJPZZZI';
+                    break;
+            }
+            await shell(`aws s3 sync --acl public-read --exclude "node_modules/*" --exclude ".git/*" ${source} s3://${destination}`);
+            if (cloudfront) {
+                await shell(`aws cloudfront create-invalidation --distribution-id ${cloudfront} --paths "/*"`)
+            }
         });
 };

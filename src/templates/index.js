155c9a340e42b776e0f23df8e19f9cdd3e649b76
Travis Tidwell <travis@form.io>
2023-06-07 11:45:31 -0500

    Change compress to compose

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
-const compose = _.template(fs.readFileSync(path.join(__dirname, 'compress/docker-compose.yml.tpl')));
+const compose = _.template(fs.readFileSync(path.join(__dirname, 'compose/docker-compose.yml.tpl')));
 const nginx = _.template(fs.readFileSync(path.join(__dirname, 'nginx/default.conf.tpl')));
 module.exports = {
     compose: (package, options) => compose({package, options}).replace(/\n+/g, "\n"),
